d269b603a59092f12d1b3c56b885333d9242aa4f
Misael-Micciche-JCMaxwell-5Ai <misael.micciche@jcmaxwell.it>
2016-12-07 09:01:55 +0100

    Aggiornamento codice
    Aggiunta e controllo commenti

--- Server/ServerTestoMultiThreaded.java
+++ Server/ServerTestoMultiThreaded.java
@@ -12,26 +12,30 @@ import java.net.*;
 import java.io.*;
 
 public class ServerTestoMultiThreaded {
+    //DICHIARAZIONE LISTA CONTENENTE IL NOME DEGLI UTENTI
     static List<SocketWorker> listaSocket = new ArrayList();
     public static void main(String[] args) {
-
+        //SE LA STRINGA NON E' UNIVOCA 
         if (args.length != 1) {
+            //STAMPA L'USO CORRETTO DEL COMANDO
             System.out.println("Uso: java ServerTestoMultithreaded <Porta Server>");
             return;
         }
-
+        //SE E' UNIVOCA LEGGE IL NUMERO DI PORTA INSERITO
         int portNumber = Integer.parseInt(args[0]);
 
-
         try{
+            //CREA L'OGGETTO SERVER DI TIPO SERVER SOCKET PASSANDOGLI IL NUMERO DI PORTA
             ServerSocket server = new ServerSocket(portNumber);
             System.out.println("Server di Testo in esecuzione...  (CTRL-C quits)\n");
-
+            
+            //CICLO INFINITO
             while(true){
                 SocketWorker w;
                 try {
                     //server.accept returns a client connection
                     w = new SocketWorker(server.accept());
+                    //AGGIUNGE L'OGGETTO ALLA LISTA
                     listaSocket.add(w);
                     Thread t = new Thread(w);
                     t.start();
